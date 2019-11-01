0a94fbea62c3e6761754b038557be5b014c17908
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2019-11-01 15:56:32 +0100

    First import

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mac_destktop_image
 More flexible desktop image cycler
+
+## Installation
+ * Clone the repo where you want
+ * make sure you have the following packages installed and usable by python:
+    - ruamel.yaml
+    - sqlite3
+ * copy `desktop_image.yml.example` as `desktop_image.yml` and edit with your preferences
+
+### With pyenv
+My method of choice of python scripts is using [pyenv](https://github.com/pyenv/pyenv). This script was tested with version 3.5.4 but it might work with other versions. Add a `.python-version` file in your installation directory and, provided the required packages are installed in that version, the script should work.
+
+## Usage
+The command is the script `dimg`. 
+
+### Single shot
+By default, the script changes your desktop image. The image is chosen randomly from the directories listed in your configuration file. J
---
+++ desktop_image.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+# Attempt to go directly into the sqlite db as in 
+# https://stackoverflow.com/questions/33533304/change-scaling-for-all-desktop-backgrounds-on-mac-via-a-script
+
+import glob
+import os
+from pathlib import Path
+import random
+import sqlite3
+from subprocess import run
+import sys
+from ruamel.yaml import YAML
+
+# Colors:
+#   - black   {0, 0, 0}
+#   - white   {100, 100, 100}
+#   - grey    {50, 50, 50}
+#   - dark    {20, 20, 20}
+#   - brown   {30, 7, 15}
+#   - green   {18, 24, 15}
+
+# Scalings:
+#   - fit:    Stretches the image to fit the desktop's width and height;
+#   - tile:   Repeat image to fill screen;
+#   - centre: Centres the image without enlarging, however it 
+#             will reduce the size of an image that would 
+#             otherwise spill outside the boundary of the desktop;
+#   - best:   Enlarges or reduces the size of the image to occupy 
+#             the maximum possible area of the desktop without 
+#             going outside the desktop boundary and preserving the 
+#             aspect ratio of the image, i.e. a proportional size adjustment;
+#   - fill:   Sizes the image to cover the full area of the desktop, 
+#             preserving the aspect ratio, but extending beyond the boundary 
+#             of the desktop along one of its axes if necessary in order 
+#             to cover the background.
+
+# This could be read from a yaml file
+
+CONFIG_PATH = Path(sys.argv[0]).with_suffix('.yml')
+CONFIG = YAML(typ='safe').load(CONFIG_PATH)
+
+SCALINGS={
+  "fill": 1,
+  "tile": 2,
+  "center": 3,
+  "best": 4,
+  "fit": 5,
+}
+
+COLORS={
+  "black": [0.0, 0.0, 0.0],
+  "white": [1.0, 1.0, 1.0],
+  "grey": [0.5, 0.5, 0.5],
+  "dark": [0.2, 0.2, 0.2],
+  "brown": [0.30, 0.7, 0.15],
+  "green": [0.18, 0.24, 0.15],
+}
+
+def weighted_choice(choices):
+  total = sum(c["weight"] for c in choices)
+  r = random.uniform(0, total)
+  upto = 0
+  for c in choices:
+    w = c["weight"]
+    upto += w
+    if upto >= r:
+      return c
+  assert False, "Shouldn't get here"
+
+# --------------------------------------------------------------------- main
+dirs=[]
+for d in CONFIG["dirs"]:
+  m = CONFIG["defaults"].copy()
+  m.update(d)
+  if (not os.path.isabs(m["dir"])):
+    m["dir"] = CONFIG["base"] + m["dir"]
+  dirs.append(m)
+
+random.seed()
+
+# test weighted_choice
+# for i in range(0, 100):
+#   d = weighted_choice(CONFIG["dirs"])["dir"]
+#   print(d)
+
+dd = weighted_choice(dirs)
+# dd = {"dir": "Test", "scaling": "best", "bgcolor": "black"}
+d = dd["dir"]
+ff=glob.glob(d + "/*.jpg") + glob.glob(d + "/*.png")
+f=random.sample(ff, 1)[0]
+bf=os.path.basename(f)
+s=SCALINGS[dd["scaling"]]
+c=COLORS[dd["bgcolor"]]
+
+dbpath = str( Path.home() / Path('Library/Application Support/Dock/desktoppicture.db'))
+
+# print("directory: %s" % d)
+# print("basename:  %s" % bf)
+# print("scaling:   %s" % dd["scaling"])
+# print("bgcolor:   %4.2f, %4.2f, %4.2f" % (c[0], c[1],  c[2]))
+# print("dbpath:    %s" % dbpath)
+
+# Preference key vs meaning:
+#  1: Image path
+#  2: Scaling method (Fill Screen (1), Tile (2), Center (3), Stretch to Fill Screen (4), Fit to Screen (5))
+#  3: Fill color
+#  4: Fill color
+#  5: Fill color
+#  9: Enable automatic changing
+# 10: Directory path to images
+# 11: Image changing interval
+# 12: Random order
+# 16: Current image (used when automatic changing is enabled)
+
+conn = sqlite3.connect(dbpath)
+curs = conn.cursor()
+
+curs.executescript('''
+  DELETE FROM data;
+  DELETE FROM displays;
+  DELETE FROM pictures;
+  DELETE FROM preferences;
+  DELETE FROM prefs;
+  DELETE FROM spaces;
+  INSERT INTO pictures (space_id, display_id) VALUES (null, null);
+''')
+conn.commit()
+curs.execute("INSERT INTO data (value) VALUES ('%s')" % f)     #  1 Image path      (option key  1)
+curs.execute("INSERT INTO data (value) VALUES (%d)" % s)       #  2 Scaling         (option key  2)
+curs.execute("INSERT INTO data (value) VALUES (%4.2f)" % c[0]) #  3 Color Red       (option key  3)
+curs.execute("INSERT INTO data (value) VALUES (%4.2f)" % c[1]) #  4 Color Green     (option key  4)
+curs.execute("INSERT INTO data (value) VALUES (%4.2f)" % c[2]) #  5 Color Blue      (option key  5)
+curs.execute("INSERT INTO data (value) VALUES (0)")            #  6 Auto change     (option key  9)
+curs.execute("INSERT INTO data (value) VALUES ('%s')" % d)     #  7 Image dir       (option key 10)
+curs.execute("INSERT INTO data (value) VALUES (30)")           #  8 Change interval (option key 11)
+curs.execute("INSERT INTO data (value) VALUES (0)")            #  9 Random order    (option key 12)
+curs.execute("INSERT INTO data (value) VALUES ('%s')" % bf)    # 10 Current image   (option key 16)
+conn.commit()
+curs.executescript('''
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (1, 1, 1);
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (2, 2, 1);
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (3, 3, 1);
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (4, 4, 1);
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (5, 5, 1);
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (9, 6, 1);
+  INSERT INTO preferences (key, data_id, picture_id) VALUES (12, 9, 1);
+''')
+conn.commit()
+
+# TODO: find a way to avoid this!!
+run(["killall", "Dock"])
